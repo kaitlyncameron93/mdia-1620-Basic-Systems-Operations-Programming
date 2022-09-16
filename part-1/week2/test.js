a3ad9d6148a0d57888356230a012565789096694
Kaitlyn Cameron <kcameron37@se14l0120-20.ad.bcit.ca>
2022-09-16 14:45:44 -0700

    new code

---
+++ part-1/week2/test.js
@@ -0,0 +1,11 @@
+//Declare
+//Declare  a variable clled electronic devices 
+
+//Camel Case 
+var electronicDevices;
+
+//pascal case 
+var ElectronicDevices;
+
+//snake case 
+var Electonic_Devices;
